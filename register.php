58205b64d8e5467e095d73f4dfdfae5b4310e836
Anz555 <114560496+Anz555@users.noreply.github.com>
2022-10-17 16:08:35 +0300

    Create register.php

---
+++ register.php
@@ -0,0 +1,37 @@
+<?php
+    session_start();
+    include 'config.php';
+    $username = $_POST['username'];
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $email = $_POST['email'];
+    $pass = $_POST['pass'];
+    $pass2 = $_POST['repeatpass'];
+
+
+    if(empty($username) || empty($name) || empty($surname) || empty($email) || empty($pass) || empty($pass2)) {
+        exit('required');
+    }
+    $sql = "select username from users where username = '" . $username . "'";
+    $result = $conn->query($sql);
+
+    
+    if ($result->num_rows > 0) {
+        exit('taken');
+    } else if(strcmp($pass, $pass2) != 0) {
+        exit('notmatch');
+    } else if(strtolower($pass) == $pass ||  !preg_match('#[0-9]#',$pass)) {
+        exit('letternumber');
+    } else {
+        $sql = 'insert into users(username, name,surname, email, pass) VALUES ("'.$username.'","'.$name.'","'.$surname.'","'.$email.'","'.md5($pass).'")';
+        $result = $conn->query($sql);
+        if(!isset($_POST['userid'])){
+            $sql = "select id from users where username = '".$username."'";
+            $result = $conn->query($sql);
+            $fetch_check = $result->fetch_object();
+            $user_id = $fetch_check->id;
+            $_SESSION["user_id"] = $user_id;
+        }
+    }
+    
+?>
