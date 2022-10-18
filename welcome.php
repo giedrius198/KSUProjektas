f0c5244448a7e83eceed349a8eb05755519fc173
AnastasijaBogatova <aaa.bogatova@gmail.com>
2022-10-18 14:39:54 +0300

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -1,15 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
-    <title>Wellcome</title>
 
-    <style></style>
-
+        <style></style>
     </head>
     <body>
-        
         <div style="text-align: center">
             <h1>Wellcome</h1>
             <p>You must be logged in or registered user to use all features.</p>
@@ -17,15 +12,6 @@
             <button type="button" id="Reg" class="btn btn-dark">Register</button>
         </div>
 
-    <script type="text/javascript">
-        document.getElementById("logIn").onclick = function () {
-            location.href = "login.php";
-        };
-        document.getElementById("Reg").onclick = function () {
-            location.href = "register.php";
-            sessionStorage.setItem("OpenReg", "true");
-        };
-    </script>
 
     </body>
 </html> 
