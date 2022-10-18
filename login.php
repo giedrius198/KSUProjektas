02b985383284c364a8a89c1aa1289f664c2e9e67
AnastasijaBogatova <aaa.bogatova@gmail.com>
2022-10-18 15:26:48 +0300

    Update login.php

--- login.php
+++ login.php
@@ -33,7 +33,7 @@
             $email = $_POST['email'];
             $password = $_POST['password'];
             
-            $sql="SELECT * FROM users WHERE email='$email' AND password='$password'";
+            $sql="SELECT * FROM users WHERE email='$email' AND password='".md5($password)."'";
             $result = $conn->query($sql);
                 if ($result->num_rows == 0) {
                     $login_err ="Neteisingas el.paštas arba slaptažodis";
