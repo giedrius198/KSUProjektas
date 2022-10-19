1d1b80e74694883a2bf5e43aa694fa4833faf053
AnastasijaBogatova <aaa.bogatova@gmail.com>
2022-10-19 07:38:28 +0300

    Updated Nav Bar

--- index.php
+++ index.php
@@ -17,6 +17,43 @@
                 text-decoration: none;
                 color: initial;
             }
+
+            nav{
+                position: relative;
+            }
+
+            
+
+            li {
+                display: block;
+                transition-duration: 0.5s;
+            }
+
+             li:hover {
+                cursor: pointer;
+            }
+
+            ul li ul {
+                visibility: hidden;
+                opacity: 0;
+                position: absolute;
+                transition: all 0.5s ease;
+                margin-top: 1rem;
+                left: 0;
+                display: none;
+            }
+
+            ul li:hover > ul,
+            ul li ul:hover {
+                visibility: visible;
+                opacity: 1;
+                display: block;
+            }
+
+            ul li ul li {
+                clear: both;
+                width: 100%;
+            }
         </style>
 
     </head>
@@ -24,9 +61,40 @@
         <header>
             <a href="welcome.php"  class="btn btn-primary">Prisijungti</a> 
         </header>
-        <main>
-            <h1>MAIN PAGE</h1>
-        </main>
+        
+        <nav role="navigation">
+			<ul>
+				<li><a href="#">Pirma komanda</a>
+                    <ul class="dropdown">
+                        <li><a href="#">Sub-1</a></li>
+                        <li><a href="#">Sub-2</a></li>
+                        <li><a href="#">Sub-3</a></li>
+                    </ul>
+                </li>
+				<li><a href="#">Antra komanda</a>
+                    <ul class="dropdown">
+                        <li><a href="#">Sub-1</a></li>
+                        <li><a href="#">Sub-2</a></li>
+                        <li><a href="#">Sub-3</a></li>
+                    </ul>
+                </li>
+				<li><a href="#">Apie</a>
+                    <ul class="dropdown">
+                        <li><a href="#">Sub-1</a></li>
+                        <li><a href="#">Sub-2</a></li>
+                        <li><a href="#">Sub-3</a></li>
+                    </ul>
+                </li>
+			</ul>
+		</nav>
+
+        <h1>MAIN PAGE</h1>
+        
+        <div class="content">
+            <article>
+            </article>    
+        <div>
+
         <footer></footer>
     </body>
 </html>
