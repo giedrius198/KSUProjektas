7f492cfdc1d6fd6d8be9fea4e19371fc98b0b607
AnastasijaBogatova <aaa.bogatova@gmail.com>
2022-10-22 21:31:42 +0300

    css

--- login.php
+++ login.php
@@ -54,9 +54,6 @@
     </head>
     <body>
         <header>
-            <div class="header">
-                <a href="#default" class="logo">CompanyLogo</a>
-            </div> 
         </header>
         <main>
             <div class="wrapper">
