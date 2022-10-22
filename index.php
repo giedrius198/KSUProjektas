c3b6e32d7731de99c4166b6c4b52cfebce25d055
AnastasijaBogatova <aaa.bogatova@gmail.com>
2022-10-22 16:21:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Home page</title>
 
-        <link rel="stylesheet" type="text/css" href="css\main.css">
+        <link rel="stylesheet" type="text/css" href="css\main.css">        
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
     </head>
@@ -29,7 +29,7 @@
 			<ul class="navbar">
 				<li><a class="active" href="index.php">HOME</a></li>
 				<li><a href="#">ABOUT</a></li>
-				<li><a href="#">item 3</a></li>
+				<li><a href="#">ARTICLES</a></li>
                 <li style="float:right"><input type="text" placeholder="IEŠKOTI..."></li>
 			</ul>
 		</nav>
